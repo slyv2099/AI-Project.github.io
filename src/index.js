99eb2ef327b0e513e8099955cf7591e3633e3166
Matt <matthew.huang0208@gmail.com>
2025-05-26 19:42:14 +0700

    Set up GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect,
-} from 'react-router-dom'
-
-import './style.css'
-import NotFound1, { NotFound } from './views/not-found'
-import Home from './views/home'
+} from 'react-router-dom';
+import './style.css';
+import NotFound from './views/not-found'; // Corrected import
+import Home from './views/home';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={NotFound1} exact path="/not-found" />
         <Route component={Home} exact path="/" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
+        <Route component={NotFound} exact path="/not-found" />
+        <Route component={NotFound} path="*" /> {/* Catch-all route */}
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
